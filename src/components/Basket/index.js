c386fed7d009fc8fd8e839425ea2b88586dbae18
ZamirZhiianbekov <zamirbekaripovich@gmail.com>
2023-05-18 21:39:11 +0300

    add new text

--- src/components/Basket/index.js
+++ src/components/Basket/index.js
@@ -25,7 +25,7 @@ const Basket = () => {
                         }
                     </th>
                     <th scope="col" className="px-6 py-3">
-                        Action
+                        Action delete
                     </th>
                 </tr>
                 </thead>
